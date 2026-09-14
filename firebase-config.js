f6ac1c0c1410d26f8753293c04672d48abcbcdb0
green ninja <navarjun.karthick@gmail.com>
2026-09-14 21:12:27 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCjy1FWd8ek8McTJSQ6NCbGfExRisHDawg",
-  authDomain: "studyflow-14.firebaseapp.com",
-  projectId: "studyflow-14",
-  storageBucket: "studyflow-14.firebasestorage.app",
-  messagingSenderId: "565453862285",
-  appId: "1:565453862285:web:f27c5dadb3862c8b02f9c4",
-  measurementId: "G-23B518556D"
-};
